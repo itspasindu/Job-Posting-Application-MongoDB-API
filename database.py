71553f3088459d442b36ed5409678d95c1f26ddd
Pasindu Dewviman <pasindudewviman59@gmail.com>
2025-12-01 20:16:27 +0530

    Refactor database interactions to use specific collections for companies, jobs, and applicants

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ MONGO_URL = "mongodb://127.0.0.1:27017"
 
 client = AsyncIOMotorClient(MONGO_URL)
 
-db = client["job-posting-API"]   # Database name
-collection = db["company"]    # Collection name
-collection = db["job-posting"] 
-collection = db["applicants"] 
+db = client["job-posting-application-API"]   # Database name
+companies_collection = db["companies"]          # for company data
+jobs_collection = db["job_postings"]            # for job postings
+applicants_collection = db["applicants"]        # for applicants
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from bson import ObjectId
-from database import collection
-from models import User, UserDB
+from database import companies_collection, jobs_collection, applicants_collection
 from models import company, companyDB, jobPosting, jobPostingDB, applicants, applicantsDB
 
 app = FastAPI(title="Job Posting Application API With MongoDB")
@@ -10,7 +9,7 @@ app = FastAPI(title="Job Posting Application API With MongoDB")
 @app.post("/company", response_model=companyDB)
 async def create_company(Company: company):
     company_dict = Company.dict()
-    result = await collection.insert_one(company_dict)
+    result = await companies_collection.insert_one(company_dict)
     company_dict["_id"] = str(result.inserted_id)
     return company_dict
 
@@ -18,7 +17,7 @@ async def create_company(Company: company):
 @app.post("/jobs", response_model=jobPostingDB)
 async def create_jobs(jobs: jobPosting):
     jobs_dict = jobs.dict()
-    result = await collection.insert_one(jobs_dict)
+    result = await jobs_collection.insert_one(jobs_dict)
     jobs_dict["_id"] = str(result.inserted_id)
     return jobs_dict
 
@@ -26,7 +25,7 @@ async def create_jobs(jobs: jobPosting):
 @app.post("/applicants", response_model=applicantsDB)
 async def create_applicants(candidates: applicants):
     candidates_dict = candidates.dict()
-    result = await collection.insert_one(candidates_dict)
+    result = await applicants_collection.insert_one(candidates_dict)
     candidates_dict["_id"] = str(result.inserted_id)
     return candidates_dict
 
@@ -34,7 +33,7 @@ async def create_applicants(candidates: applicants):
 @app.get("/company", response_model=list[companyDB])
 async def get_company():
     companies = []
-    async for company in collection.find():
+    async for company in companies_collection.find():
         company["_id"] = str(company["_id"])
         companies.append(company)
     return companies
@@ -43,42 +42,42 @@ async def get_company():
 @app.get("/jobs", response_model=list[jobPostingDB])
 async def get_jobs():
     jobs = []
-    async for job in collection.find():
+    async for job in jobs_collection.find():
         job["_id"] = str(job["_id"])
         jobs.append(job)
     return jobs
 
 # Read all applicants
-@app.get("/applicants", response_model=list[companyDB])
+@app.get("/applicants", response_model=list[applicantsDB])
 async def get_applicant():
     applicants = []
-    async for applicant in collection.find():
+    async for applicant in applicants_collection.find():
         applicant["_id"] = str(applicant["_id"])
         applicants.append(applicant)
     return applicants
 
 # read company by id
-@app.get("/company/company_id}", response_model=companyDB)
+@app.get("/company/{company_id}}", response_model=companyDB)
 async def get_company(company_id: str):
-    company = await collection.find_one({"_id": ObjectId(company_id)})
+    company = await companies_collection.find_one({"_id": ObjectId(company_id)})
     if not company:
         raise HTTPException(404, "User not found")
     company["_id"] = str(company["_id"])
     return company
 
 # read job posting by id
-@app.get("/jobs/job_id}", response_model=jobPostingDB)
+@app.get("/jobs/{job_id}}", response_model=jobPostingDB)
 async def get_jobs(job_id: str):
-    job = await collection.find_one({"_id": ObjectId(job_id)})
+    job = await jobs_collection.find_one({"_id": ObjectId(job_id)})
     if not job:
         raise HTTPException(404, "User not found")
     job["_id"] = str(job["_id"])
     return job
 
 # read applicants by id
-@app.get("/applicants/candidate_id}", response_model=applicantsDB)
+@app.get("/applicants/{candidate_id}}", response_model=applicantsDB)
 async def get_applicant(applicant_id: str):
-    applicant = await collection.find_one({"_id": ObjectId(applicant_id)})
+    applicant = await applicants_collection.find_one({"_id": ObjectId(applicant_id)})
     if not applicant:
         raise HTTPException(404, "User not found")
     applicant["_id"] = str(applicant["_id"])
@@ -87,20 +86,20 @@ async def get_applicant(applicant_id: str):
 # update company details
 @app.put("/company/{company_id}", response_model=companyDB)
 async def update_company(company_id: str, Company: company):
-    result = await collection.update_one(
+    result = await companies_collection.update_one(
         {"_id": ObjectId(company_id)},
         {"$set": Company.dict()}
     )
     if result.modified_count == 0:
         raise HTTPException(404, "User not found")
-    updated = await collection.find_one({"_id": ObjectId(company_id)})
+    updated = await companies_collection.find_one({"_id": ObjectId(company_id)})
     updated["_id"] = str(updated["_id"])
     return updated
 
 # delete company details
 @app.delete("/company/{company_id}")
 async def delete_company(company_id: str):
-    result = await collection.delete_one({"_id": ObjectId(company_id)})
+    result = await companies_collection.delete_one({"_id": ObjectId(company_id)})
     if result.deleted_count == 0:
         raise HTTPException(404, "company not found")
     return {"message": "company deleted successfully"}
